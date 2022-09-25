eda3e5051f514901f33315ccca2c77d67289f7b2
Edgar Sakwa <sakwaedgar57@gmail.com>
2022-09-25 15:37:41 +0300

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print("{}".format(result))
+    sum = 0
+    for x in range(len(sys.argv) - 1):
+        sum += int(sys.argv[x + 1])
+    print(sum)
+    
